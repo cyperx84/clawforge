bede57866dc72413b586f880fa45712e1be48a4d
cyperx <cyperx84@gmail.com>
2026-03-15 17:22:53 +1000

    chore: clean up Formula, prune legacy tests, verify fleet-status/cost/logs

--- Formula/clawforge.rb
+++ Formula/clawforge.rb
@@ -3,33 +3,23 @@ class Clawforge < Formula
   homepage "https://github.com/cyperx84/clawforge"
   url "https://github.com/cyperx84/clawforge/archive/refs/tags/v2.1.0.tar.gz"
   sha256 "8a330d297211cd2a5543b73d959046673c8ee0ec9870eff4bffd8fcf50706c5d"
-  sha256 "9568edc52e32c7afb648fb9f63c548d97aa829cd371099feb680388668cd85e8"
-  sha256 "577cdf0e2cac9030e6ef20e6ba6a582c3a810bb43043b4fbc44c288996f514d7"
   license "MIT"
   head "https://github.com/cyperx84/clawforge.git", branch: "main"
 
   depends_on "jq"
-  depends_on "tmux"
-  depends_on "gh"
 
   def install
-    # Install shell scripts
     libexec.install Dir["bin/*"]
     libexec.install Dir["lib"]
-    libexec.install Dir["tui"]
     libexec.install Dir["config"]
     libexec.install "VERSION"
     libexec.install "registry"
 
-    # Create wrapper that sets CLAWFORGE_DIR
     (bin/"clawforge").write <<~EOS
       #!/bin/bash
       export CLAWFORGE_DIR="#{libexec}"
       exec "#{libexec}/clawforge" "$@"
     EOS
-
-    # Dashboard binary
-    bin.install libexec/"clawforge-dashboard"
   end
 
   test do
